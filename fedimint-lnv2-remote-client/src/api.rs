b253b8afc9c950cbf0828c13ad8f46dd20ce76c8
Tommy Volk <tvolk131@gmail.com>
2025-01-21 14:51:02 -0600

    chore: remove unused send-side LightningFederationApi code

--- fedimint-lnv2-remote-client/src/api.rs
+++ fedimint-lnv2-remote-client/src/api.rs
@@ -9,19 +9,14 @@ use fedimint_core::task::{MaybeSend, MaybeSync};
 use fedimint_core::util::SafeUrl;
 use fedimint_core::{apply, async_trait_maybe_send, NumPeersExt, PeerId};
 use fedimint_lnv2_common::endpoint_constants::{
-    AWAIT_INCOMING_CONTRACT_ENDPOINT, AWAIT_PREIMAGE_ENDPOINT, CONSENSUS_BLOCK_COUNT_ENDPOINT,
-    GATEWAYS_ENDPOINT,
+    AWAIT_INCOMING_CONTRACT_ENDPOINT, GATEWAYS_ENDPOINT,
 };
 use fedimint_lnv2_common::ContractId;
 use rand::seq::SliceRandom;
 
 #[apply(async_trait_maybe_send!)]
 pub trait LightningFederationApi {
-    async fn consensus_block_count(&self) -> FederationResult<u64>;
-
     async fn await_incoming_contract(&self, contract_id: &ContractId, expiration: u64) -> bool;
-
-    async fn await_preimage(&self, contract_id: &ContractId, expiration: u64) -> Option<[u8; 32]>;
 
     async fn gateways(&self) -> FederationResult<Vec<SafeUrl>>;
 
@@ -33,14 +28,6 @@ impl<T: ?Sized> LightningFederationApi for T
 where
     T: IModuleFederationApi + MaybeSend + MaybeSync + 'static,
 {
-    async fn consensus_block_count(&self) -> FederationResult<u64> {
-        self.request_current_consensus(
-            CONSENSUS_BLOCK_COUNT_ENDPOINT.to_string(),
-            ApiRequestErased::new(()),
-        )
-        .await
-    }
-
     // TODO: Use `request_current_consensus_retry` like `fedimint-lnv2-client` does
     // once v0.6.0 is released.
     async fn await_incoming_contract(&self, contract_id: &ContractId, expiration: u64) -> bool {
@@ -54,18 +41,6 @@ where
             .unwrap();
 
         contract_id.is_some()
-    }
-
-    // TODO: Use `request_current_consensus_retry` like `fedimint-lnv2-client` does
-    // once v0.6.0 is released.
-    async fn await_preimage(&self, contract_id: &ContractId, expiration: u64) -> Option<[u8; 32]> {
-        self.request_with_strategy(
-            ThresholdConsensus::new(self.all_peers().to_num_peers()),
-            AWAIT_PREIMAGE_ENDPOINT.to_string(),
-            ApiRequestErased::new((contract_id, expiration)),
-        )
-        .await
-        .unwrap()
     }
 
     async fn gateways(&self) -> FederationResult<Vec<SafeUrl>> {
