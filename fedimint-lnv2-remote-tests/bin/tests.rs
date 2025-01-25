cb5a8771f801fd75228375f94cb4a8271a99bd3c
Tommy Volk <tvolk131@gmail.com>
2025-01-25 11:21:06 -0600

    feat: remove iroh

--- fedimint-lnv2-remote-tests/bin/tests.rs
+++ fedimint-lnv2-remote-tests/bin/tests.rs
@@ -1,15 +1,15 @@
-use std::time::Duration;
-
 use devimint::cmd;
+use devimint::devfed::DevJitFed;
 use devimint::federation::Client;
 use fedimint_core::core::OperationId;
 use fedimint_core::secp256k1::PublicKey;
 use fedimint_core::Amount;
 use fedimint_lnv2_common::contracts::IncomingContract;
-use fedimint_lnv2_remote_client::{FinalRemoteReceiveOperationState, PublicKeys};
+use fedimint_lnv2_common::ContractId;
+use fedimint_lnv2_remote_client::FinalRemoteReceiveOperationState;
 use lightning_invoice::Bolt11Invoice;
 use substring::Substring;
-use tracing::{error, info};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -26,59 +26,34 @@ async fn main() -> anyhow::Result<()> {
         )
         .await?;
 
-        let receiver_client = fed.new_joined_client("client0").await?;
-        let claimer_client = fed.new_joined_client("client1").await?;
-
-        let receiver_pubkeys = get_public_keys(&receiver_client).await.unwrap();
-        let claimer_pubkeys = get_public_keys(&claimer_client).await.unwrap();
-
         let ldk_gw_addr = dev_fed.gw_cln().await.as_ref().unwrap().addr.clone();
 
-        register_claimer(&receiver_client, &claimer_pubkeys)
-            .await
-            .unwrap();
-
-        let (invoice, incoming_contract, operation_id) = remote_receive(
-            &receiver_client,
-            &claimer_client,
-            Amount::from_sats(1_000),
+        info!("Testing happy path");
+        test_happy_path(
+            &dev_fed,
+            fed.new_joined_client("client0").await?,
+            fed.new_joined_client("client1").await?,
             &ldk_gw_addr,
         )
-        .await
-        .unwrap();
+        .await?;
 
-        dev_fed
-            .lnd()
-            .await
-            .unwrap()
-            .pay_bolt11_invoice(invoice.to_string())
-            .await
-            .unwrap();
+        info!("Testing syncing many payments");
+        test_syncing_many_payments(
+            &dev_fed,
+            fed.new_joined_client("client2").await?,
+            fed.new_joined_client("client3").await?,
+            &ldk_gw_addr,
+        )
+        .await?;
 
-        tokio::task::spawn(async move {
-            run_remote_receiver_server(&receiver_client).await.unwrap();
-        });
-
-        info!(
-            "Claimer client balance is {}",
-            claimer_client.balance().await.unwrap()
-        );
-
-        info!("Syncing with remote receiver...");
-        let sync_res = sync_with_remote_receiver(&claimer_client, receiver_pubkeys.iroh_pk).await;
-
-        if sync_res.is_ok() {
-            info!("Claimer synced with remote receiver!");
-        } else {
-            error!("Failed to sync with remote receiver. {sync_res:?}");
-        }
-
-        std::thread::sleep(Duration::from_secs(5));
-
-        info!(
-            "Claimer client balance is now {}",
-            claimer_client.balance().await.unwrap()
-        );
+        info!("Testing claim idempotency");
+        test_claim_idempotency(
+            &dev_fed,
+            fed.new_joined_client("client4").await?,
+            fed.new_joined_client("client5").await?,
+            &ldk_gw_addr,
+        )
+        .await?;
 
         info!("Successfully completed fedimint-lnv2-remote test");
 
@@ -87,81 +62,189 @@ async fn main() -> anyhow::Result<()> {
     .await
 }
 
-async fn get_public_keys(client: &Client) -> anyhow::Result<PublicKeys> {
+async fn test_happy_path(
+    dev_fed: &DevJitFed,
+    receiver_client: Client,
+    claimer_client: Client,
+    gw_addr: &str,
+) -> anyhow::Result<()> {
+    let claimer_pk = get_public_key(&claimer_client).await?;
+
+    let (invoice, operation_id) = remote_receive(
+        &receiver_client,
+        claimer_pk,
+        &Amount::from_msats(1_000_000),
+        gw_addr,
+    )
+    .await?;
+
+    let claimable_contracts = get_claimable_contracts(&receiver_client, claimer_pk).await?;
+    assert!(claimable_contracts.is_empty());
+
+    dev_fed
+        .lnd()
+        .await
+        .unwrap()
+        .pay_bolt11_invoice(invoice.to_string())
+        .await
+        .unwrap();
+
+    await_remote_receive(&receiver_client, operation_id).await?;
+
+    let claimable_contracts = get_claimable_contracts(&receiver_client, claimer_pk).await?;
+    assert_eq!(claimable_contracts.len(), 1);
+    let incoming_contract_hex = claimable_contracts[0].clone();
+
+    claim_contract(&claimer_client, &incoming_contract_hex).await?;
+
+    let incoming_contract: IncomingContract =
+        bincode::deserialize(&hex::decode(incoming_contract_hex).unwrap()).unwrap();
+
+    remove_claimed_contract(
+        &receiver_client,
+        claimer_pk,
+        incoming_contract.contract_id(),
+    )
+    .await?;
+
+    let claimable_contracts = get_claimable_contracts(&receiver_client, claimer_pk).await?;
+    assert!(claimable_contracts.is_empty());
+
+    assert_eq!(claimer_client.balance().await.unwrap(), 943_055);
+
+    Ok(())
+}
+
+async fn test_syncing_many_payments(
+    dev_fed: &DevJitFed,
+    receiver_client: Client,
+    claimer_client: Client,
+    gw_addr: &str,
+) -> anyhow::Result<()> {
+    const INVOICES_COUNT: usize = 20;
+
+    let claimer_pk = get_public_key(&claimer_client).await?;
+
+    let mut invoices = Vec::new();
+    for _ in 0..INVOICES_COUNT {
+        invoices.push(
+            remote_receive(
+                &receiver_client,
+                claimer_pk,
+                &Amount::from_msats(1_000_000),
+                gw_addr,
+            )
+            .await?,
+        );
+    }
+
+    let claimable_contracts = get_claimable_contracts(&receiver_client, claimer_pk).await?;
+    assert!(claimable_contracts.is_empty());
+
+    for (invoice, _operation_id) in &invoices {
+        dev_fed
+            .lnd()
+            .await
+            .unwrap()
+            .pay_bolt11_invoice(invoice.to_string())
+            .await
+            .unwrap();
+    }
+
+    for (_invoice, operation_id) in invoices {
+        await_remote_receive(&receiver_client, operation_id).await?;
+    }
+
+    let claimable_contracts = get_claimable_contracts(&receiver_client, claimer_pk).await?;
+    assert_eq!(claimable_contracts.len(), INVOICES_COUNT);
+
+    for i in 0..INVOICES_COUNT {
+        claim_contract(&claimer_client, &claimable_contracts[i]).await?;
+
+        let incoming_contract: IncomingContract =
+            bincode::deserialize(&hex::decode(&claimable_contracts[i]).unwrap()).unwrap();
+
+        // TODO: Test removing contracts in bulk. This needs to be piped through the
+        // CLI.
+        remove_claimed_contract(
+            &receiver_client,
+            claimer_pk,
+            incoming_contract.contract_id(),
+        )
+        .await?;
+    }
+
+    let claimable_contracts = get_claimable_contracts(&receiver_client, claimer_pk).await?;
+    assert!(claimable_contracts.is_empty());
+
+    assert_eq!(
+        claimer_client.balance().await.unwrap(),
+        943_055 * INVOICES_COUNT as u64
+    );
+
+    Ok(())
+}
+
+async fn test_claim_idempotency(
+    dev_fed: &DevJitFed,
+    receiver_client: Client,
+    claimer_client: Client,
+    gw_addr: &str,
+) -> anyhow::Result<()> {
+    let claimer_pk = get_public_key(&claimer_client).await?;
+
+    let (invoice, operation_id) = remote_receive(
+        &receiver_client,
+        claimer_pk,
+        &Amount::from_msats(1_000_000),
+        gw_addr,
+    )
+    .await?;
+
+    dev_fed
+        .lnd()
+        .await
+        .unwrap()
+        .pay_bolt11_invoice(invoice.to_string())
+        .await
+        .unwrap();
+
+    await_remote_receive(&receiver_client, operation_id).await?;
+
+    let claimable_contracts = get_claimable_contracts(&receiver_client, claimer_pk).await?;
+    assert_eq!(claimable_contracts.len(), 1);
+    let incoming_contract_hex = claimable_contracts[0].clone();
+
+    for _ in 0..20 {
+        claim_contract(&claimer_client, &incoming_contract_hex).await?;
+    }
+
+    assert_eq!(claimer_client.balance().await.unwrap(), 943_055);
+
+    Ok(())
+}
+
+async fn get_public_key(client: &Client) -> anyhow::Result<PublicKey> {
     Ok(serde_json::from_value(
-        cmd!(client, "module", "lnv2", "get-public-keys")
+        cmd!(client, "module", "lnv2", "get-public-key")
             .out_json()
             .await?,
     )?)
-}
-
-async fn run_remote_receiver_server(client: &Client) -> anyhow::Result<()> {
-    Ok(serde_json::from_value(
-        cmd!(client, "module", "lnv2", "run-remote-receiver-server")
-            .out_json()
-            .await?,
-    )?)
-}
-
-async fn sync_with_remote_receiver(
-    client: &Client,
-    remote_receiver_iroh_pubkey: iroh::PublicKey,
-) -> anyhow::Result<()> {
-    Ok(cmd!(
-        client,
-        "module",
-        "lnv2",
-        "sync-with-remote-receiver",
-        remote_receiver_iroh_pubkey
-    )
-    .run()
-    .await?)
-}
-
-async fn register_claimer(client: &Client, claimer_pks: &PublicKeys) -> anyhow::Result<()> {
-    Ok(cmd!(
-        client,
-        "module",
-        "lnv2",
-        "register-claimer",
-        claimer_pks.claimer_static_pk,
-        claimer_pks.iroh_pk
-    )
-    .run()
-    .await?)
-}
-
-async fn unregister_claimer(
-    client: &Client,
-    claimer_static_pk: PublicKey,
-    force: bool,
-) -> anyhow::Result<()> {
-    Ok(cmd!(
-        client,
-        "module",
-        "lnv2",
-        "unregister-claimer",
-        claimer_static_pk,
-        force
-    )
-    .run()
-    .await?)
 }
 
 async fn remote_receive(
     receiver_client: &Client,
-    claimer_client: &Client,
-    amount: Amount,
+    claimer_pk: PublicKey,
+    amount: &Amount,
     gateway: &str,
-) -> anyhow::Result<(Bolt11Invoice, IncomingContract, OperationId)> {
-    let claimer_pks = get_public_keys(claimer_client).await?;
-
+) -> anyhow::Result<(Bolt11Invoice, OperationId)> {
     let mut json_result = cmd!(
         receiver_client,
         "module",
         "lnv2",
         "remote-receive",
-        claimer_pks.claimer_static_pk,
+        claimer_pk,
+        "--amount",
         amount.to_string(),
         "--gateway",
         gateway
@@ -171,26 +254,22 @@ async fn remote_receive(
 
     let json_array = json_result.as_array_mut().unwrap();
 
-    assert_eq!(json_array.len(), 3);
+    assert_eq!(json_array.len(), 2);
 
     let operation_id: OperationId = json_array.pop().unwrap().as_str().unwrap().parse().unwrap();
-    let incoming_contract: IncomingContract =
-        serde_json::from_value(json_array.pop().unwrap()).unwrap();
     let invoice: Bolt11Invoice = json_array.pop().unwrap().as_str().unwrap().parse().unwrap();
 
-    Ok((invoice, incoming_contract, operation_id))
+    Ok((invoice, operation_id))
 }
 
-async fn await_remote_receive_funded(
-    client: &Client,
-    operation_id: OperationId,
-) -> anyhow::Result<()> {
+async fn await_remote_receive(client: &Client, operation_id: OperationId) -> anyhow::Result<()> {
     assert_eq!(
         cmd!(
             client,
             "module",
             "lnv2",
-            "await-remote-receive-funded",
+            "await-remote-receive",
+            "--operation-id",
             serde_json::to_string(&operation_id)?.substring(1, 65)
         )
         .out_json()
@@ -200,4 +279,51 @@ async fn await_remote_receive_funded(
     );
 
     Ok(())
+}
+
+async fn get_claimable_contracts(
+    client: &Client,
+    claimer_pk: PublicKey,
+) -> anyhow::Result<Vec<String>> {
+    Ok(serde_json::from_value(
+        cmd!(
+            client,
+            "module",
+            "lnv2",
+            "get-claimable-contracts",
+            claimer_pk
+        )
+        .out_json()
+        .await?,
+    )?)
+}
+
+async fn remove_claimed_contract(
+    client: &Client,
+    claimer_pk: PublicKey,
+    contract_id: ContractId,
+) -> anyhow::Result<()> {
+    Ok(cmd!(
+        client,
+        "module",
+        "lnv2",
+        "remove-claimed-contract",
+        claimer_pk,
+        "--contract-id",
+        contract_id.0
+    )
+    .run()
+    .await?)
+}
+
+async fn claim_contract(client: &Client, incoming_contract_hex: &str) -> anyhow::Result<()> {
+    Ok(cmd!(
+        client,
+        "module",
+        "lnv2",
+        "claim-contract",
+        incoming_contract_hex
+    )
+    .run()
+    .await?)
 }
