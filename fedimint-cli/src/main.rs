a678aed5a9b697cd7b1d85630a0ee086646f1540
Tommy Volk <tvolk131@gmail.com>
2025-02-06 09:39:49 -0600

    chore: remove unneeded modules from test CLI

--- fedimint-cli/src/main.rs
+++ fedimint-cli/src/main.rs
@@ -1,19 +1,17 @@
 use fedimint_cli::FedimintCli;
 use fedimint_core::fedimint_build_code_version_env;
-use fedimint_ln_client::LightningClientInit;
-use fedimint_meta_client::MetaClientInit;
+use fedimint_lnv2_remote_client::LightningRemoteClientInit;
 use fedimint_mint_client::MintClientInit;
 use fedimint_wallet_client::WalletClientInit;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     FedimintCli::new(fedimint_build_code_version_env!())?
-        .with_module(LightningClientInit::default())
         .with_module(MintClientInit)
         .with_module(WalletClientInit::default())
-        .with_module(MetaClientInit)
-        .with_module(fedimint_lnv2_remote_client::LightningRemoteClientInit::default())
+        .with_module(LightningRemoteClientInit::default())
         .run()
         .await;
+
     Ok(())
 }
